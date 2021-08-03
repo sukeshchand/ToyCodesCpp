bfe819180207791be8b9e1071013732693dfefd3
sukeshchand <sukesh.chand@bridge-global.com>
2021-08-03 14:39:23 +0530

    StringArrayWithVectorInput

--- main.cpp
+++ main.cpp
@@ -4,12 +4,35 @@
 
 using namespace std;
 
-int main()
+void StringArrayWithVectorView()
 {
-    vector<string> msg {"Hello", "C++", "World", "from", "VS Code", "and the C++ extension!"};
-    for (const string& word : msg)
+    vector<string> msg{"Hello", "C++", "World", "from", "VS Code", "and the C++ extension!"};
+    for (const string &word : msg)
     {
-        cout << word << " ";
+        cout << word << endl;
     }
     cout << endl;
 }
+
+void StringArrayWithVectorInput()
+{
+    vector<string> msg {};
+    string inputString;
+    for (size_t i = 0; i < 5; i++)
+    {
+        cin >> inputString;
+        msg.push_back(inputString);
+    }
+    cout << "--------------Output----------------------";
+    for (size_t i = 0; i < 5; i++)
+    {
+        cout << msg[i] << endl;
+    }
+}
+
+int main()
+{
+    // StringArrayWithVectorView();
+    StringArrayWithVectorInput();
+}
+
